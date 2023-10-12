c2a733c6257682ad427bbaf0529e96b1c4c01b91
Littlezoski <shicheng.zhou@my.utsa.edu>
2023-10-12 00:25:45 -0500

    ecommerce

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import Checkout from "./scenes/checkout/Checkout";
+import Home from "./scenes/home/Home";
+import ItemDetails from "./scenes/itemDetails/ItemDetails";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
+
+function App() {
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="item/:itemId" element={<ItemDetails />}/>
+          <Route path="checkout" element={<Checkout />}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
